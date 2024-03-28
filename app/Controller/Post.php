c7bd548b0b6ae5d751aff4bac0cd0c9de26d63bf
NiviroWils <145367856+NiviroWils@users.noreply.github.com>
2024-03-28 17:22:15 +0700

    Начало работы над реализацией функции регистрации

---
+++ app/Controller/Post.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Controller;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+}
