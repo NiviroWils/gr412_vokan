67d0e67216bbe511027d93f03b74db29c1419de0
NiviroWils <145367856+NiviroWils@users.noreply.github.com>
2024-04-05 22:14:49 +0700

    Продолжение настройки Middleware для ролей

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,20 @@ use Src\Route;
 
 Route::add('GET', '/hello', [Controller\Site::class, 'hello']);
 Route::add(['GET', 'POST'], '/signup', [Controller\Site::class, 'signup'])
-    ->middleware('role2');
+    ->middleware('role2','auth');
 Route::add(['GET', 'POST'], '/login', [Controller\Site::class, 'login']);
 Route::add('GET', '/logout', [Controller\Site::class, 'logout']);
 Route::add(['GET', 'POST'], '/newdivision', [Controller\Site::class, 'newdivision'])
-    ->middleware('role');
+    ->middleware('role', 'auth');
 Route::add(['GET', 'POST'], '/newroom', [Controller\Site::class, 'newroom'])
-    ->middleware('role');
+    ->middleware('role', 'auth');
 Route::add(['GET', 'POST'],'/newsub', [Controller\Site::class, 'newsub'])
-    ->middleware('role');
+    ->middleware('role', 'auth');
 Route::add(['GET', 'POST'],'/newphone', [Controller\Site::class, 'newphone'])
     ->middleware('role');
 Route::add('GET', '/divisions', [Controller\Site::class, 'divisions'])
-    ->middleware('role');
+    ->middleware('role', 'auth');
 Route::add('GET', '/rooms', [Controller\Site::class, 'rooms'])
-    ->middleware('role');
+    ->middleware('role', 'auth');
 Route::add('GET', '/subscribers', [Controller\Site::class, 'subscribers'])
-    ->middleware('role');
+    ->middleware('role', 'auth');
